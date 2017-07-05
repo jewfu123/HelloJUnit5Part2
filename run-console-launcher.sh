764c3ebbea4ca0210d0aea77f3d607940e3a85c5
J Steven Perry <jstevenperry@gmail.com>
2017-07-05 10:52:06 -0500

    Changed JUnit JARs to M5

--- run-console-launcher.sh
+++ run-console-launcher.sh
@@ -7,13 +7,13 @@
 CONSOLE_LAUNCHER_JAR_ROOT=./target/lib
 echo CONSOLE_LAUNCHER_JAR_ROOT=${CONSOLE_LAUNCHER_JAR_ROOT}
 #
-CONSOLE_LAUNCHER_JAR=junit-platform-console-standalone-1.0.0-M4.jar
+CONSOLE_LAUNCHER_JAR=junit-platform-console-standalone-1.0.0-M5.jar
 echo CONSOLE_LAUNCHER_JAR=${CONSOLE_LAUNCHER_JAR}
 #
 LIB_ROOT=./target/lib
 echo LIB_ROOT=${LIB_ROOT}
 #
-DEPENDENT_JAR_CLASSPATH=${LIB_ROOT}/spring-core-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-context-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-beans-4.3.8.RELEASE.jar:${LIB_ROOT}/commons-logging-1.2.jar:${LIB_ROOT}/spring-aop-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-expression-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-jdbc-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-tx-4.3.8.RELEASE.jar:${LIB_ROOT}/log4j-1.2.17.jar:${LIB_ROOT}/derby-10.13.1.1.jar:${LIB_ROOT}/junit-platform-runner-1.0.0-M4.jar:${LIB_ROOT}/commons-lang3-3.5.jar
+DEPENDENT_JAR_CLASSPATH=${LIB_ROOT}/spring-core-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-context-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-beans-4.3.8.RELEASE.jar:${LIB_ROOT}/commons-logging-1.2.jar:${LIB_ROOT}/spring-aop-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-expression-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-jdbc-4.3.8.RELEASE.jar:${LIB_ROOT}/spring-tx-4.3.8.RELEASE.jar:${LIB_ROOT}/log4j-1.2.17.jar:${LIB_ROOT}/derby-10.13.1.1.jar:${LIB_ROOT}/junit-platform-runner-1.0.0-M5.jar:${LIB_ROOT}/commons-lang3-3.5.jar
 echo DEPENDENT_JAR_CLASSPATH=${DEPENDENT_JAR_CLASSPATH}
 #
 TEST_CLASSES_ROOT=./target/classes:./target/test-classes/
