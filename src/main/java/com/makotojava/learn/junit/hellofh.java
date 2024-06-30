d4dfed07f5604347e25f43defc954d1dd69240c0
jewfu123 <jewfu_123@163.com>
2024-07-01 07:25:59 +0900

    update 7-5 added

---
+++ src/main/java/com/makotojava/learn/junit/hellofh.java
@@ -0,0 +1,10 @@
+package com.makotojava.learn.junit;
+
+public class hellofh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello fu hong is superman!");
+	}
+
+}
